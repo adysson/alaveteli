addca5f4dced757c018814c0397f113ec2ca0f28
Gareth Rees <gareth@mysociety.org>
2015-06-30 10:59:28 +0100

    Discard mail above the configured spam threshold

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -221,6 +221,17 @@ class RequestMailer < ApplicationMailer
       return
     end
 
+    spam_threshold = AlaveteliConfiguration.incoming_email_spam_threshold
+    spam_header = AlaveteliConfiguration.incoming_email_spam_header
+    spam_score = email.header[spam_header].try(:value).to_f
+
+    if spam_header && spam_threshold && spam_score
+      if spam_score > spam_threshold
+        # Do nothing. Silently drop spam above the threshold
+        return
+      end
+    end
+
     # Send the message to each request, to be archived with it
     for reply_info_request in reply_info_requests
       # If environment variable STOP_DUPLICATES is set, don't send message with same id again
